4e31e1afc3f61926aafc00d94b01e94d7b1fdb10
anuraagpolisetty <anuraag@uw.edu>
2020-02-18 23:13:04 -0800

    Updates

---
+++ precache-manifest.45fb3bd36af97b00e86b1c468b2802e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c84e1cd589698dd0c69b8e2c63a6b98",
+    "url": "/tanc/index.html"
+  },
+  {
+    "revision": "edd461a517ce44935b98",
+    "url": "/tanc/static/css/main.432eb040.chunk.css"
+  },
+  {
+    "revision": "14ca2b2fd5997b1f80d3",
+    "url": "/tanc/static/js/2.0b13ad88.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/tanc/static/js/2.0b13ad88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd461a517ce44935b98",
+    "url": "/tanc/static/js/main.3163a5c5.chunk.js"
+  },
+  {
+    "revision": "d498bcc2012903736876",
+    "url": "/tanc/static/js/runtime-main.228afc46.js"
+  },
+  {
+    "revision": "7ee3186a28c2b985d947a0a455a07919",
+    "url": "/tanc/static/media/AnuraagTemp.7ee3186a.jpg"
+  },
+  {
+    "revision": "471c7f614b11a772df76f5b6e60b0dd2",
+    "url": "/tanc/static/media/ChrisTemp.471c7f61.jpg"
+  },
+  {
+    "revision": "6efbfe79059fdac985fb3c64ecee5dde",
+    "url": "/tanc/static/media/NavedTemp.6efbfe79.jpg"
+  },
+  {
+    "revision": "b4d9c2d45d0211e48a08222cf161efdf",
+    "url": "/tanc/static/media/Scene1.b4d9c2d4.png"
+  },
+  {
+    "revision": "571daa042ef5d8e1092dee22aec08d3a",
+    "url": "/tanc/static/media/Scene2.571daa04.png"
+  },
+  {
+    "revision": "3bb950f58850ad2fd7bf5f95ae6faac3",
+    "url": "/tanc/static/media/Scene3.3bb950f5.png"
+  },
+  {
+    "revision": "39ae7b9b3c0e76e247b355ef6af15f91",
+    "url": "/tanc/static/media/TabithaTemp.39ae7b9b.jpg"
+  }
+]);
